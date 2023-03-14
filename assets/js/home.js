091faf3ae0756b4f47069a3991bd1812d0ad45f1
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-03-14 11:03:16 -0300

    fix: cleaning code with dynamic cards

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,32 +1,33 @@
-let htmlCode = ``;
-getData().events.forEach(function(element){
-    htmlCode =
-    htmlCode +
-    `
-    <div class="card m-2 shadow" style="width: 18rem">
-    <img
-      id="cardImage"
-      src="${element.image}"
-      class="card-img-top"
-      alt="${element.name}"
-      height="150"
-    />
-    <div class="card-body">
-      <h5 class="card-title" id="cardTitle">${element.name}</h5>
-      <p class="card-text" id="cardText">
-      ${element.description}
-      </p>
-    </div>
-    <div class="card-footer border-0">
-      <div class="d-flex justify-content-between">
-        <p id="cardPrice">$${element.price}</p>
-        <a href="./templates/detail.html" class="btn amazeButton"
-          >See more...</a
-        >
-      </div>
-    </div>
-  </div>
-  `;
-});
-const cards = document.getElementById('cards');
-cards.innerHTML = htmlCode;
+// let htmlCode = ``;
+// getData().events.forEach(function(element){
+//     htmlCode =
+//     htmlCode +
+//     `
+//     <div class="card m-2 shadow" style="width: 18rem">
+//     <img
+//       id="cardImage"
+//       src="${element.image}"
+//       class="card-img-top"
+//       alt="${element.name}"
+//       height="150"
+//     />
+//     <div class="card-body">
+//       <h5 class="card-title" id="cardTitle">${element.name}</h5>
+//       <p class="card-text" id="cardText">
+//       ${element.description}
+//       </p>
+//     </div>
+//     <div class="card-footer border-0">
+//       <div class="d-flex justify-content-between">
+//         <p id="cardPrice">$${element.price}</p>
+//         <button href="./templates/detail.html" class="btn amazeButton" 
+//           >See more...</button
+//         >
+//       </div>
+//     </div>
+//   </div>
+//   `;
+// });
+// const cards = document.getElementById('cards');
+// cards.innerHTML = htmlCode;
+getCards(getData().events);
--- assets/js/main.js
+++ assets/js/main.js
@@ -193,6 +193,39 @@ function getCategories() {
   const checks = document.getElementById('checks');
   checks.innerHTML = htmlCheckboxes;
 }
-
+function getCards(data) {
+  let htmlCode = ``;
+  data.forEach(function(element){
+    htmlCode =
+    htmlCode +
+    `
+    <div class="card m-2 shadow" style="width: 18rem">
+    <img
+      id="cardImage"
+      src="${element.image}"
+      class="card-img-top"
+      alt="${element.name}"
+      height="150"
+    />
+    <div class="card-body">
+      <h5 class="card-title" id="cardTitle">${element.name}</h5>
+      <p class="card-text" id="cardText">
+      ${element.description}
+      </p>
+    </div>
+    <div class="card-footer border-0">
+      <div class="d-flex justify-content-between">
+        <p id="cardPrice">$${element.price}</p>
+        <button href="./templates/detail.html" class="btn amazeButton" 
+          >See more...</button
+        >
+      </div>
+    </div>
+  </div>
+  `;
+});
+const cards = document.getElementById('cards');
+cards.innerHTML = htmlCode;
+};
 const categories = ["Food Fair", "Museum", "Costume Party","Music Concert","Race","Book Exchange","Cinema"];
 getCategories();
